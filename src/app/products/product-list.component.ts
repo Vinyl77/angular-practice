b442006a6f2725e6013b7ed33ac212716fc7c2cf
Richard Yvarra <richard.yvarra@ibm.com>
2022-01-19 13:35:07 -0500

    added new component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,54 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
-
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls:['./product-list.component.css']
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
-  private _listFilter: string ='';
-  get listFilter(): string{
+  private _listFilter: string = '';
+  get listFilter(): string {
     return this._listFilter;
   }
-  set listFilter(value: string){
+  set listFilter(value: string) {
     this._listFilter = value;
-    console.log('In setter', value);
+    console.log('In setter:', value);
     this.filteredProducts = this.performFilter(value);
   }
-
-
 
   filteredProducts: IProduct[] = [];
   products: IProduct[] = [];
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService) {}
 
-  performFilter(filterBy: string): IProduct[]{
+  performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: IProduct)=>
-    product.productName.toLocaleLowerCase().includes(filterBy));
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
+
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-   
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
-  onRatingClicked(message: string): void{
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
   }
-
-
 }
